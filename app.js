87785fde36a34b715c102b1bc35384a3ee67e8d9
Денис Диденко <denisdidenko@MacBook-Pro-Denis.local>
2020-07-22 10:34:03 +0300

    express-graphql

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schemas');
+
+const app = express();
+
+app.use(cors());
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log('server started'));
