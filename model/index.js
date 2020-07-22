0496204d361a327a81391faa83b20f83e7d33934
Денис Диденко <denisdidenko@MacBook-Pro-Denis.local>
2020-07-22 14:35:14 +0300

    update model

--- model/index.js
+++ model/index.js
@@ -3,11 +3,34 @@ const csv = require('csvtojson/v2');
 
 const csvFilePath = path.join(__dirname + '/lps.csv');
 
-function getData() {
+function readData() {
   return csv({
     output: 'json',
     delimiter: ';',
   }).fromFile(csvFilePath);
 }
 
-module.exports.getData = getData;
+// init database
+const db = readData().then((data) => {
+  return data.reduce((acc, item) => {
+    const landing = acc.find((lp) => lp.name === item.name);
+    if (landing) {
+      landing.version = item.version;
+      return acc;
+    }
+
+    acc.push(item);
+
+    return acc;
+  }, []);
+});
+
+const getAllLps = () => db;
+const getLpById = (id) => db.find((landing) => landing.lid === id);
+const getLpsByIds = (ids) => db.filter((landing) => ids && ids.includes(landing.lid));
+
+module.exports = {
+  getAllLps,
+  getLpById,
+  getLpsByIds,
+};
--- resolvers/index.js
+++ resolvers/index.js
@@ -1,31 +1,10 @@
-const { getData } = require('../model');
-
-// init database
-const db = getData().then((data) => {
-  return data.reduce((acc, item) => {
-    const landing = acc.find((lp) => lp.name === item.name);
-    if (landing) {
-      landing.version = item.version;
-      return acc;
-    }
-
-    acc.push(item);
-
-    return acc;
-  }, []);
-});
+const { getAllLps, getLpById, getLpsByIds } = require('../model');
 
 const resolvers = {
   Query: {
-    allLps() {
-      return db;
-    },
-    lpsByIds(parent, args) {
-      return db.filter((landing) => args.ids && args.ids.includes(landing.lid));
-    },
-    lp(parent, args) {
-      return db.find((landing) => landing.lid === args.id);
-    },
+    allLps: () => getAllLps(),
+    lpsByIds: (parent, { ids }) => getLpsByIds(ids),
+    lp: (parent, { id }) => getLpById(id),
   },
 };
 
